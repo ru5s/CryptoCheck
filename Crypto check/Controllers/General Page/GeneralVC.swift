d08c783b7889b28fd56849b2e4ba747ecf13f9d2
Ruslan <hellperock@gmail.com>
2023-06-18 16:59:25 +0500

    start project

--- Crypto check/Controllers/General Page/GeneralVC.swift
+++ Crypto check/Controllers/General Page/GeneralVC.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GeneralVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
 
 
