a935313d3c90d456b44ff2df98ab4f4d0a301c2e
Ruslan <hellperock@gmail.com>
2023-06-18 18:08:24 +0500

    create tabbar

--- Crypto check/Controllers/General Page/GeneralVC.swift
+++ Crypto check/Controllers/General Page/GeneralVC.swift
@@ -13,7 +13,9 @@ class GeneralVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .red
+        view.backgroundColor = .systemGray3
+        
+        navigationController?.isNavigationBarHidden = true
     }
 
 
---
+++ Crypto check/Tab bar/TabBar.swift
@@ -0,0 +1,61 @@
+//
+//  TabBarVC.swift
+//  Crypto check
+//
+//  Created by Ruslan Ismailov on 18/06/23.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    let firstPage = GeneralVC()
+    let secondPage = GeneralVC()
+    let thirdPage = GeneralVC()
+    
+    var navigationColor = UIColor.white
+    var tabBarActiveColor = UIColor.white
+    var tabBarPassiveColor = UIColor.gray
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.tintColor = tabBarActiveColor
+        tabBar.unselectedItemTintColor = tabBarPassiveColor
+        
+        setupVCs()
+    }
+    
+    func setupVCs(){
+        viewControllers = [
+            createNavController(for: firstPage, title: "All coins", image: UIImage(systemName: "bonjour")!),
+            createNavController(for: secondPage, title: "Charts", image: UIImage(systemName: "chart.line.uptrend.xyaxis")!),
+            createNavController(for: thirdPage, title: "Settings", image: UIImage(systemName: "slider.vertical.3")!),
+        ]
+        
+    }
+    
+    fileprivate func createNavController(for rootViewController: UIViewController,
+                                                      title: String,
+                                                      image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = false
+        navController.navigationController?.hidesBarsOnSwipe = true
+        
+        navController.navigationBar.titleTextAttributes = [.foregroundColor: navigationColor as Any]
+        navController.navigationBar.isTranslucent = true
+        
+        navController.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: #selector(backBtn))
+        
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+    
+    @objc private func backBtn(){
+        dismiss(animated: true)
+    }
+    
+
+}
