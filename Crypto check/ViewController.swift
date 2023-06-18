38ff2915aecf5e562c597002655b0d1121807d9e
Ruslan <hellperock@gmail.com>
2023-06-18 16:41:59 +0500

    Initial Commit

---
+++ Crypto check/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto check
+//
+//  Created by Ruslan Ismailov on 18/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
